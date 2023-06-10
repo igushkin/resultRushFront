bcf20a894ac9d8d87ddea5136cd8b8582e6c6f80
Mark <igushkin.m@yandex.ru>
2023-06-10 19:30:29 +0100

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import 'simplebar';
 
 import {AppComponent} from './app.component';
+import { SidebarComponent } from './views/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
